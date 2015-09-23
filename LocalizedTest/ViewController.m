a774b6150d11631384597372c57881f84c2617e1
Cloudox <cloudox@foxmail.com>
2015-09-23 22:00:20 +0800

    实现App名称、内容文本在不同语言下（中文、英文）显示不同的内容，并且可以获取设备当前的语言

--- LocalizedTest/ViewController.m
+++ LocalizedTest/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 使用NSLocalizedString来调用Localization文件中设置的内容实现本地化
+    // 第一个参数是要显示的内容对应的key值，第二个参数是注释，暂时没发现有什么用
+    self.locallizedLabel.text = NSLocalizedString(@"LabelText", nil);
+    
+    // 获取所支持的语言
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSArray *languages = [defaults objectForKey:@"AppleLanguages"];
+    NSLog(@"%@", languages);
+    
+    // 获取当前使用的语言
+    NSArray *preferredLanguages = [NSLocale preferredLanguages];
+    NSString *currentLanguages = [preferredLanguages objectAtIndex:0];
+    NSLog(@"Current Languages:%@", currentLanguages);
 }
 
 - (void)didReceiveMemoryWarning {
